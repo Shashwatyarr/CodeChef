edaa2ba8bf88335828d7ea6e8c42c352a1b653d2
Shashwat Srivastava <shashwat.2428it2297@kiet.edu>
2025-10-30 21:52:51 +0530

    29/10/2025

---
+++ starters210/q3.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+
+#define f first
+#define s second
+#define pb push_back
+#define mp make_pair
+#define rep(i, a, b) for (int i = a; i <= b; i++)
+#define rev(i, a, b) for (int i = a; i >= b; i--)
+#define all(x) x.begin(), x.end()
+#define sz(x) ((int)x.size())
+#define getv(v, n)              \
+    for (int i = 0; i < n; i++) \
+    {                           \
+        int x;                  \
+        cin >> x;               \
+        v.pb(x);                \
+    }
+#define getlv(v, n)             \
+    for (int i = 0; i < n; i++) \
+    {                           \
+        ll x;                   \
+        cin >> x;               \
+        v.pb(x);                \
+    }
+
+void fastio()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+}
+
+int main()
+{
+    fastio();
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int a, b;
+        cin >> a >> b;
+        if (a != b)
+            cout << a + b << endl;
+        else
+            cout << a + b - 1 << endl;
+    }
+    return 0;
+}
