07d5d8998913e894106197595669e54533d31e95
Shashwat Srivastava <shashwat.2428it2297@kiet.edu>
2025-10-30 21:51:03 +0530

    29/10/2025

---
+++ starters210/q1.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+
+#define f first
+#define s second
+#define pb push_back
+#define mp make_pair
+#define rep(i, a, b) for (int i = a; i <= b; i++)
+#define rev(i, a, b) for (int i = a; i >= b; i--)
+#define all(x) x.begin(), x.end()
+#define sz(x) ((int)x.size())
+#define getv(v, n)              \
+    for (int i = 0; i < n; i++) \
+    {                           \
+        int x;                  \
+        cin >> x;               \
+        v.pb(x);                \
+    }
+#define getlv(v, n)             \
+    for (int i = 0; i < n; i++) \
+    {                           \
+        ll x;                   \
+        cin >> x;               \
+        v.pb(x);                \
+    }
+
+void fastio()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+}
+
+int main()
+{
+    fastio();
+    int a, b;
+    cin >> a >> b;
+    cout << a * b * 100;
+    return 0;
+}
